6f560b0ad216776d67b9923ef0f3f1e41448ebb0
Praveen Phatak <praveenphatak@gmail.com>
2020-09-10 11:21:24 +0530

    Static Files

---
+++ static/ipl_app/js/app.js
@@ -0,0 +1,57 @@
+$(document).ready(function () {
+    new WOW().init();
+
+    $('.mid').on('click', function () {
+
+        if ($(this).find('.switch-input').prop('checked') == true) {
+            $(this).find('.switch-input').prop('checked', false);
+            var $text = $(this).find('.switch-label').data('off');
+            $(this).closest('.match-row').find('.bet-form').find("input[name='team']").val($text);
+        } else {
+            $(this).find('.switch-input').prop('checked', true);
+            var $text = $(this).find('.switch-label').data('on');
+            $(this).closest('.match-row').find('.bet-form').find("input[name='team']").val($text);
+        }
+    });
+
+    $('.switch-input').each(function () {
+        $(this).closest('.mid').find('div').text('Select Team');
+        if ($(this).prop('checked') == true) {
+            var $text = $(this).parent().find('.switch-label').data('on');
+            $(this).closest('.match-row').find('.bet-form').find("input[name='team']").val($text);
+        } else {
+            var $text = $(this).parent().find('.switch-label').data('off');
+            $(this).closest('.match-row').find('.bet-form').find("input[name='team']").val($text);
+        }
+    });
+
+    $('.navbar-toggler').on('click', function () {
+        if ($(this).find('.cross').css('visibility') == 'hidden') {
+            $(this).find('.default').css('visibility', 'hidden');
+            $(this).find('.cross').css('visibility', 'visible');
+        } else {
+            $(this).find('.default').css('visibility', 'visible');
+            $(this).find('.cross').css('visibility', 'hidden');
+        }
+    });
+
+    $('.btn-bet').on('click', function () {
+        $(this).closest('.match-row').find('.bet-row').toggleClass('show');
+        $(this).closest('.btn-row').find('.btn-winner').toggleClass('show');
+        $(this).toggleClass('show');
+    });
+
+    $('.btn-winner').on('click', function () {
+        $(this).closest('.match-row').find('.winner-row').toggleClass('show');
+        $(this).closest('.btn-row').find('.btn-bet').toggleClass('show');
+        $(this).toggleClass('show');
+    });
+
+
+});
+
+$(function () {
+    $(".fold-table tr.view").on("click", function () {
+        $(this).toggleClass("open").next(".fold").toggleClass("open");
+    });
+});
